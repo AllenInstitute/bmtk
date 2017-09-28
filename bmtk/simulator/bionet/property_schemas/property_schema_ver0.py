bdd9a434c0e583cd3a75507f532aff0c99c28aa2
kaeld <kaeld@alleninstitute.org>
2017-09-28 11:32:10 -0700

    Fixing licensing info.

--- bmtk/simulator/bionet/property_schemas/property_schema_ver0.py
+++ bmtk/simulator/bionet/property_schemas/property_schema_ver0.py
@@ -1,7 +1,7 @@
 # Allen Institute Software License - This software license is the 2-clause BSD license plus clause a third
 # clause that prohibits redistribution for commercial purposes without further permission.
 #
-# Copyright 20XX. Allen Institute. All rights reserved.
+# Copyright 2017. Allen Institute. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 # following conditions are met:
