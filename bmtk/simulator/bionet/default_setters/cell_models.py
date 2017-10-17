b3b8293d5304f0407a45139d9995c088b69c8533
kaeld <kaeld@alleninstitute.org>
2017-10-17 15:20:08 -0700

    Adding all-active cell loader function

--- bmtk/simulator/bionet/default_setters/cell_models.py
+++ bmtk/simulator/bionet/default_setters/cell_models.py
@@ -37,7 +37,6 @@ Functions will be loaded by bionetwork and called when a new cell object is crea
 loaded with Cell-Types json files or their NeuroML equivelent, but may be overridden by the users.
 """
 
-
 def IntFire1(node):
     """Loads a point integrate and fire neuron"""
     model_params = node.model_params
@@ -73,12 +72,91 @@ def Biophys1_dict(cell):
     """ Set parameters for cells from the Allen Cell Types database Prior to setting parameters will replace the
     axon with the stub
     """
-
     morphology_file = cell['morphology_file']
     hobj = h.Biophys1(str(morphology_file))
     fix_axon(hobj)
     set_params_peri(hobj, cell.model_params)
     return hobj
+
+
+def Biophys1AllActive(cell_prop):
+    morphology_file_name = str(cell_prop['morphology_file'])
+    hobj = h.Biophys1(morphology_file_name)
+    fix_axon_allactive(hobj)
+    # set_params_peri(hobj, cell_prop.model_params)
+    set_params(hobj, cell_prop.model_params)
+    return hobj
+
+
+def set_params(hobj, params_dict):
+    # params_dict = json.load(open(params_file_name, 'r'))
+    passive = params_dict['passive'][0]
+    genome = params_dict['genome']
+    conditions = params_dict['conditions'][0]
+
+    section_map = {}
+    for sec in hobj.all:
+        section_name = sec.name().split(".")[1][:4]
+        if section_name in section_map:
+            section_map[section_name].append(sec)
+        else:
+            section_map[section_name] = [sec]
+
+    for sec in hobj.all:
+        sec.insert('pas')
+        # sec.insert('extracellular')
+
+    if 'e_pas' in passive:
+        print('setting e_pas')
+        e_pas_val = passive['e_pas']
+        for sec in hobj.all:
+            for seg in sec:
+                seg.pas.e = e_pas_val
+
+    if 'ra' in passive:
+        print('Setting ra = {}'.format(passive['ra']))
+        ra_val = passive['ra']
+        for sec in hobj.all:
+            sec.Ra = ra_val
+
+    if 'cm' in passive:
+        print('Setting cm')
+        for cm_dict in passive['cm']:
+            cm = cm_dict['cm']
+            for sec in section_map.get(cm_dict['section'], []):
+                sec.cm = cm
+
+    for genome_dict in genome:
+        g_section = genome_dict['section']
+        if genome_dict['section'] == 'glob':
+            print("WARNING: There is a section called glob, probably old json file")
+            continue
+
+        g_value = float(genome_dict['value'])
+        g_name = genome_dict['name']
+        g_mechanism = genome_dict.get("mechanism", "")
+        for sec in section_map.get(g_section, []):
+            if g_mechanism != "":
+                print('Adding mechanism {} to {}'.format(g_mechanism, g_section))
+                sec.insert(g_mechanism)
+            setattr(sec, g_name, 3.9) #g_value)
+
+        print('setting {} to {} in {}'.format(g_name, g_value, g_section))
+
+    for erev in conditions['erev']:
+        erev_section = erev['section']
+        erev_ena = erev['ena']
+        erev_ek = erev['ek']
+        print('Setting ek to {} and ena to {} in {}'.format(erev_ek, erev_ena, erev_section))
+
+        if erev_section in section_map:
+            for sec in section_map.get(erev_section, []):
+                if h.ismembrane('k_ion', sec=sec) == 1:
+                    setattr(sec, 'ek', erev_ek)
+                if h.ismembrane('na_ion', sec=sec) == 1:
+                    setattr(sec, 'ena', erev_ena)
+        else:
+            print("Warning: can't set erev for {}, section array doesn't exist".format(erev_section))
 
 
 def set_params_peri(hobj, biophys_params):
@@ -141,6 +219,44 @@ def fix_axon(hobj):
     h.define_shape()
 
 
+def fix_axon_allactive(hobj):
+    """Replace reconstructed axon with a stub
+
+    Parameters
+    ----------
+    hobj: instance of a Biophysical template
+        NEURON's cell object
+    """
+    # find the start and end diameter of the original axon, this is different from the perisomatic cell model
+    # where diameter == 1.
+    axon_diams = [hobj.axon[0].diam, hobj.axon[0].diam]
+    print hobj.axon[0].diam
+    for sec in hobj.all:
+        section_name = sec.name().split(".")[1][:4]
+        if section_name == 'axon':
+            axon_diams[1] = sec.diam
+
+    for sec in hobj.axon:
+        h.delete_section(sec=sec)
+
+    h.execute('create axon[2]', hobj)
+    for index, sec in enumerate(hobj.axon):
+        sec.L = 30
+        sec.diam = axon_diams[index]  # 1
+        hobj.axonal.append(sec=sec)
+        hobj.all.append(sec=sec)  # need to remove this comment
+
+    hobj.axon[0].connect(hobj.soma[0], 0.5, 0)
+    hobj.axon[1].connect(hobj.axon[0], 1, 0)
+
+    h.define_shape()
+
+
 add_cell_model(Biophys1, 'biophysical', overwrite=False)
+add_cell_model(Biophys1, overwrite=False)
+
+
 add_cell_model(IntFire1, 'point_IntFire1', overwrite=False)
 add_cell_model(IntFire1, 'intfire', overwrite=False)
+add_cell_model(Biophys1AllActive, 'biophysical_allactive', overwrite=False)
+add_cell_model(Biophys1AllActive, overwrite=False)
