68bd3dc34014585c1faf82db4b031feab3d5d4aa
kaeld <kaeld@alleninstitute.org>
2018-01-11 17:55:09 -0800

    cleaning up

--- docs/examples/simulator/bionet/14cells_multinode/run_bionet.py
+++ docs/examples/simulator/bionet/14cells_multinode/run_bionet.py
@@ -5,6 +5,7 @@
 
 import sys, os
 import h5py
+import traceback
 import numpy as np
 from numpy.random import randint
 from neuron import h
@@ -15,6 +16,7 @@ from bmtk.simulator.bionet.simulation import Simulation
 from bmtk.analyzer.spikes_analyzer import spike_files_equal
 from bmtk.simulator.bionet.biograph import BioGraph
 from bmtk.simulator.bionet.bionetwork import BioNetwork
+from bmtk.simulator.bionet.io import print2log0
 
 from bmtk.utils.io import TabularNetwork_AI
 from bmtk.simulator.bionet.property_schemas import AIPropertySchema
@@ -42,7 +44,8 @@ def check_cellvars(gid, conf):
         # consider using np.allclose() since the percision can vary depending on neuron version
         assert (np.array_equal(results_data, expected_data))
 
-def check_ecp():
+
+def check_ecp(err_tolerance=1e-05):
     SAMPLE_SIZE = 100
     expected_h5 = h5py.File('expected/ecp.h5', 'r')
     nrows, ncols = expected_h5['ecp'].shape
@@ -53,7 +56,7 @@ def check_ecp():
 
     assert (results_h5['ecp'].shape == (nrows, ncols))
     for i, j in zip(randint(0, nrows, size=SAMPLE_SIZE), randint(0, ncols, size=SAMPLE_SIZE)):
-        assert (results_mat[i, j] == expected_mat[i, j])
+        assert abs(results_mat[i, j] - expected_mat[i, j]) < err_tolerance
 
 
 def run(config_file):
@@ -70,48 +73,29 @@ def run(config_file):
 
     net = BioNetwork.from_config(conf, graph)   # create netwosim = Simulation.from_config(conf, network=net)  rk of in NEURON
     sim = Simulation.from_config(conf, network=net)         # initialize a simulation
-    # sim.set_recordings()                        # set recordings of relevant variables to be saved as an ouput
     sim.run()                                   # run simulation
 
     if MPI_RANK == 0:
-        # Check spikes
-        assert (os.path.exists(conf['output']['spikes_ascii_file']))
-        assert (spike_files_equal(conf['output']['spikes_ascii_file'], 'expected/spikes.txt'))
+        try:
+            # Check spikes
+            print2log0('Checking spike times')
+            assert (os.path.exists(conf['output']['spikes_ascii_file']))
+            assert (spike_files_equal(conf['output']['spikes_ascii_file'], 'expected/spikes.txt'))
+            print2log0('Spikes passed!')
 
-        # Check extracellular recordings
-        check_ecp()
+            # Check extracellular recordings
+            print2log0('Checking ECP output')
+            check_ecp()
+            print2log0('ECP passed!')
 
-        # Check saved variables
-        for saved_gids in conf['node_id_selections']['save_cell_vars']:
-            check_cellvars(saved_gids, conf)
-
-    """
-    "cell_vars_dir": "$OUTPUT_DIR/cellvars",
-
-    "save_cell_vars": ["v", "cai"],
-
-    "node_id_selections": {
-        "save_cell_vars":
-    """
-
-    '''
-    # Test the results of the ecp
-    SAMPLE_SIZE = 100
-    expected_h5 = h5py.File('expected/ecp.h5', 'r')
-    nrows, ncols = expected_h5['ecp'].shape
-    expected_mat = np.matrix(expected_h5['ecp'])
-    results_h5 = h5py.File('output/ecp.h5', 'r')
-    assert ('ecp' in results_h5.keys())
-    results_mat = np.matrix(results_h5['ecp'][:])
-
-    assert (results_h5['ecp'].shape == (nrows, ncols))
-    for i, j in zip(randint(0, nrows, size=SAMPLE_SIZE), randint(0, ncols, size=SAMPLE_SIZE)):
-        assert (results_mat[i, j] == expected_mat[i, j])
-
-
-    for gid in range(0, 11):
-        assert(os.path.exists('output/cellvars/{}.h5'.format(gid)))
-    '''
+            # Check saved variables
+            print2log0('Checking NEURON saved variables')
+            for saved_gids in conf['node_id_selections']['save_cell_vars']:
+                check_cellvars(saved_gids, conf)
+            print2log0('variables passed!')
+        except AssertionError:
+            _, _, tb = sys.exc_info()
+            traceback.print_tb(tb)
 
     pc.barrier()
     nrn.quit_execution()                        # exit
