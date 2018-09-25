6832d1fc411a47caf0ea4699df90e41a0dbb4d89
kael <kael.dai@gmail.com>
2018-09-25 14:47:14 -0700

    Adding docker scripts

---
+++ docker/entry_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [[ $1 = "nrnivmodl" ]]; then
+    shift
+    nrnivmodl $@
+elif [[ $1 = "python" ]]; then
+    shift
+    python $@
+elif [[ $1 = "jupyter" ]]; then
+    shift
+    echo "HERE"
+    jupyter notebook --allow-root --ip=* --port 8888 --no-browser --notebook-dir /home/shared --NotebookApp.token=""
+else
+    python $@
+fi
