7d3b36e704c14e059b109516e59ee9cfe9409bfe
kaeldai <kaeld@alleninstitute.org>
2019-03-30 13:18:04 -0700

    fixed multipop readers and added unit tests

--- bmtk/tests/utils/reports/spike_trains/test_csv_reader.py
+++ bmtk/tests/utils/reports/spike_trains/test_csv_reader.py
@@ -12,13 +12,12 @@ def load_spike_trains(file_path):
     elif file_path.endswith('.h5'):
         return SpikeTrains.from_sonata(file_path)
 
-    elif file_path.endswith(('.nwb')):
+    elif file_path.endswith('.nwb'):
         return SpikeTrains.from_nwb(file_path)
 
 
 def test_spikes_nopopulation(file_path, pop_name):
-    spikes = load_spike_trains(file_path) # SpikeTrains.from_csv(file_path)
-    # print(spikes.populations)
+    spikes = load_spike_trains(file_path)  # SpikeTrains.from_csv(file_path)
     assert(len(spikes.populations) == 1)
     assert(spikes.populations[0] == pop_name)
     assert(len(spikes) == 124)
@@ -100,9 +99,83 @@ def test_spikes_nopopulation(file_path, pop_name):
         spike_counts += 1
     assert(spike_counts == 124)
 
+
+def test_spikes_multipop(file_path):
+    spikes = load_spike_trains(file_path)  # SpikeTrains.from_csv(file_path)
+    assert(set(spikes.populations) == set(['lgn', 'tw']))
+    assert(len(spikes) == 144434)
+
+    #import pandas as pd
+    #tmp = pd.read_csv('spike_files/spikes.multipop.csv', sep=' ')
+    #for grp_name, grp in tmp.groupby('population'): # tmp.groupby(['population', 'node_ids']):
+    #    print(grp_name, len(grp['node_ids'].unique()))
+
+    assert(len(spikes.nodes()) == 4000 + 1997)
+    # print(spikes.nodes())
+    # exit()
+    lgn_nodes = spikes.nodes(populations='lgn')
+    assert(len(lgn_nodes) == 4000)
+    assert(lgn_nodes[0][0] == 'lgn')
+    assert(isinstance(lgn_nodes[0][1], np.integer))
+
+    tw_spikes = spikes.nodes(populations=['tw'])
+    assert(len(tw_spikes) == 1997)
+    assert(tw_spikes[0][0] == 'tw')
+
+    spikes_df = spikes.to_dataframe()
+    assert(spikes_df.shape == (144434, 3))
+    assert(np.issubdtype(spikes_df['timestamps'].dtype, np.floating))
+    assert(np.issubdtype(spikes_df['node_ids'].dtype, np.integer))
+
+    assert(spikes.to_dataframe(populations='tw').shape == (21078, 3))
+    assert(spikes.to_dataframe(populations='lgn').shape == (123356, 3))
+
+    spikes_df = spikes.to_dataframe(node_ids=np.arange(100), populations='lgn', time_window=(100.0, 1500.0))
+    assert(np.max(spikes_df['node_ids']) == 99)
+    assert(np.all(spikes_df['population'].unique() == ['lgn']))
+    assert(np.min(spikes_df['timestamps']) >= 100.0) # .agg([np.min, np.max]).values <= 1500.0)
+    assert(np.max(spikes_df['timestamps']) <= 1500.0)
+
+    lgn_spike0 = spikes.get_times(0, population='lgn')
+    assert(len(lgn_spike0) == 32)
+    assert(lgn_spike0[0] == 445.539 and lgn_spike0[-1] == 2924.594)
+
+    # Check spikes iterator
+    spike_counts = 0
+    last_nid = -1
+    for spk in spikes.spikes(populations=['lgn'], sort_order=sort_order.by_id):
+        # print(spk)
+        assert(isinstance(spk[0], np.float))
+        assert(isinstance(spk[1], six.string_types))
+        assert(isinstance(spk[2], (np.int, np.integer)))
+        nid = spk[2]
+        assert(last_nid <= nid)
+        last_nid = nid
+        spike_counts += 1
+    assert(spike_counts == 123356)
+
+    last_st = -1.0
+    spike_counts = 0
+    for spk in spikes.spikes(populations=['tw', 'lgn'], sort_order=sort_order.by_time):
+        assert(isinstance(spk[0], np.float))
+        assert(isinstance(spk[1], six.string_types))
+        assert(isinstance(spk[2], (np.int, np.integer)))
+        spk_time = spk[0]
+        assert(last_st <= spk_time)
+        last_st = spk_time
+        spike_counts += 1
+    assert(spike_counts == 144434)
+
+    # assert(len(spikes.populations) == 1)
+    # assert(spikes.populations[0] == pop_name)
+    # assert(len(spikes) == 124)
+
+
 if __name__ == '__main__':
-    #test_spikes_nopopulation(file_path='spike_files/spikes.noheader.nopop.csv', pop_name=pop_na)
-    #test_spikes_nopopulation(file_path='./spike_files/spikes.one_pop.csv', pop_name='v1')
-    #test_spikes_nopopulation(file_path='spike_files/spikes.old.h5', pop_name=pop_na)
-    #test_spikes_nopopulation(file_path='spike_files/spikes.one_pop.h5', pop_name='v1')
+    test_spikes_nopopulation(file_path='spike_files/spikes.noheader.nopop.csv', pop_name=pop_na)
+    test_spikes_nopopulation(file_path='./spike_files/spikes.one_pop.csv', pop_name='v1')
+    test_spikes_nopopulation(file_path='spike_files/spikes.old.h5', pop_name=pop_na)
+    test_spikes_nopopulation(file_path='spike_files/spikes.one_pop.h5', pop_name='v1')
     test_spikes_nopopulation(file_path='spike_files/spikes.onepop.v1.0.nwb', pop_name=pop_na)
+    test_spikes_multipop('spike_files/spikes.multipop.csv')
+    test_spikes_multipop('spike_files/spikes.multipop.h5')
--- bmtk/utils/reports/spike_trains/core.py
+++ bmtk/utils/reports/spike_trains/core.py
@@ -102,16 +102,83 @@ class NWBSTReader(STReader):
         return data_df[col_timestamps].agg([np.min, np.max]).values
 
     def get_times(self, node_id, population=None, time_window=None, **kwargs):
+        if population == None:
+            population = self._population
+
         if population != self._population:
             return []
 
+        spiketimes = self._trial_grp[str(node_id)]['data'][()]
+
+        if time_window is not None:
+            spiketimes = spiketimes[(time_window[0] <= spiketimes) & (spiketimes <= time_window[1])]
+
+        return spiketimes
+
     def to_dataframe(self, node_ids=None, populations=None, time_window=None, sort_order=SortOrder.none, **kwargs):
         if self._spikes_df is None:
-            self._spikes_df[0]
+            self._spikes_df = pd.DataFrame({
+                col_timestamps: pd.Series(dtype=np.float),
+                col_population: pd.Series(dtype=np.string_),
+                col_node_ids: pd.Series(dtype=np.uint64)
+            })
+            for node_id, node_grp in self._trial_grp.items():
+                timestamps = node_grp['data'][()]
+                node_df = pd.DataFrame({
+                    col_timestamps: timestamps,
+                    col_population: self._population,
+                    col_node_ids: np.uint64(node_id)
+                })
+                self._spikes_df = self._spikes_df.append(node_df, ignore_index=True)
 
+        selected = self._spikes_df.copy()
+        mask = True
+        if populations is not None:
+            if isinstance(populations, six.string_types) or np.isscalar(populations):
+                mask &= selected[col_population] == populations
+            else:
+                mask &= selected[col_population].isin(populations)
+
+        if node_ids is not None:
+            node_ids = [node_ids] if np.isscalar(node_ids) else node_ids
+            mask &= selected[col_node_ids].isin(node_ids)
+
+        if time_window is not None:
+            mask &= (selected[col_timestamps] >= time_window[0]) & (selected[col_timestamps] <= time_window[1])
+
+        if isinstance(mask, pd.Series):
+            selected = selected[mask]
+
+        if sort_order == SortOrder.by_time:
+            selected.sort_values(by=col_timestamps, inplace=True)
+        elif sort_order == SortOrder.by_id:
+            selected.sort_values(by=col_node_ids, inplace=True)
+
+        return selected
 
     def spikes(self, node_ids=None, populations=None, time_window=None, sort_order=SortOrder.none, **kwargs):
-        raise NotImplementedError()
+        if populations is not None:
+            if np.isscalar(populations) and populations != self._population:
+                raise StopIteration
+            elif self._population not in populations:
+                raise StopIteration
+
+        if sort_order == SortOrder.by_time:
+            spikes_df = self.to_dataframe()
+            spikes_df = spikes_df.sort_values(col_timestamps)
+            for indx in spikes_df.index:
+                r = spikes_df.loc[indx]
+                # print(r)
+                yield (r[col_timestamps], r[col_population], r[col_node_ids])
+        else:
+            node_ids = np.array(self._trial_grp.keys(), dtype=np.uint64)
+            if sort_order == SortOrder.by_id:
+                node_ids.sort()
+
+            for node_id in node_ids:
+                timestamps = self._trial_grp[str(node_id)]['data']
+                for ts in timestamps:
+                    yield (ts, self._population, node_id)
 
     def __len__(self):
         if self._n_nodes is None:
@@ -161,8 +228,10 @@ class SONATASTReader(STReader):
             self._population_map[pop_na] = self._h5_handle[GRP_spikes_root]
             self._DATASET_node_ids = 'gids'
 
-        if len(self._population_map) == 1:
-            self._default_pop = list(self._population_map.keys())[0]
+        # if len(self._population_map) == 1:
+        self._default_pop = list(self._population_map.keys())  # [0]
+        # else:
+
 
         self._population_sorting_map = {}
         for pop_name, pop_grp in self._population_map.items():
@@ -192,10 +261,13 @@ class SONATASTReader(STReader):
                 indx_beg = 0
                 last_id = node_ids_ds[0]
                 for indx, cur_id in enumerate(node_ids_ds):
+                    # print cur_id
                     if cur_id != last_id:
-                        cur_id[cur_id] = slice(indx_beg, indx)
+                        # nodes_indices[last_id] = np.arange(indx_beg, indx)
+                        nodes_indices[last_id] = slice(indx_beg, indx)
                         last_id = cur_id
                         indx_beg = indx
+                # nodes_indices[last_id] = np.arange(indx_beg, indx + 1)
                 nodes_indices[last_id] = slice(indx_beg, indx + 1)  # capture the last node_id
             else:
                 nodes_indices = {int(node_id): [] for node_id in np.unique(node_ids_ds)}
@@ -212,22 +284,25 @@ class SONATASTReader(STReader):
 
     def nodes(self, populations=None):
         if populations is None:
-            populations = [self._default_pop]
+            populations = self._default_pop  # [self._default_pop]
 
         if isinstance(populations, six.string_types) or np.isscalar(populations):
             populations = [populations]
 
         node_list = []
         for pop_name, pop_grp in self._population_map.items():
+            # print(pop_name)
+            # print populations
             if pop_name in populations:
                 # TODO: Check memory profile, may be better to iterate node_ids than convert entire set
+                # print 'BLAH'
                 node_list.extend((pop_name, node_id) for node_id in np.unique(pop_grp[self._DATASET_node_ids][()]))
 
         return node_list
 
     def time_range(self, populations=None):
         if populations is None:
-            populations = [self._default_pop]
+            populations = self._default_pop  # [self._default_pop]
 
         if isinstance(populations, six.string_types) or np.isscalar(populations):
             populations = [populations]
@@ -248,7 +323,7 @@ class SONATASTReader(STReader):
 
     def to_dataframe(self, node_ids=None, populations=None, time_window=None, sort_order=SortOrder.none, **kwargs):
         if populations is None:
-            populations = [self._default_pop]
+            populations = self._default_pop  # [self._default_pop]
 
         if isinstance(populations, six.string_types) or np.isscalar(populations):
             populations = [populations]
@@ -295,12 +370,17 @@ class SONATASTReader(STReader):
 
     def get_times(self, node_id, population=None, time_window=None, **kwargs):
         if population is None:
-            population = self._default_pop
+            if len(self._default_pop) > 1:
+                raise Exception('Error: Multiple populations, must select one.')
+
+            population = self._default_pop[0]
 
         elif population not in self.populations:
             return []
 
+        # print population, self._index_nids[population].keys()
         spikes_index = self._index_nids[population][node_id]
+        # print(spikes_index)
         spike_times = self._population_map[population][DATASET_timestamps][spikes_index]
 
         if time_window is not None:
@@ -310,7 +390,7 @@ class SONATASTReader(STReader):
 
     def spikes(self, node_ids=None, populations=None, time_window=None, sort_order=SortOrder.none, **kwargs):
         if populations is None:
-            populations = [self._default_pop]
+            populations = self._default_pop  # [self._default_pop]
 
         if sort_order == SortOrder.by_id:
             for pop_name in populations:
@@ -323,8 +403,9 @@ class SONATASTReader(STReader):
                 node_ids.sort()
                 for node_id in node_ids:
                     st_indices = index_map[node_id]
-                    for st_indx in st_indices:
-                        yield timestamps_ds[st_indx], pop_name, node_id
+                    for st in timestamps_ds[st_indices]: # st_indices:
+                        # yield timestamps_ds[st_indx], pop_name, node_id
+                        yield st, pop_name, node_id
 
         elif sort_order == SortOrder.by_time:
             index_ranges = []
@@ -338,26 +419,42 @@ class SONATASTReader(STReader):
                     index_ranges.append([pop_name, 0, len(ts_ds), np.arange(len(ts_ds)), pop_grp, ts_ds[0]])
                 else:
                     ts_ds = pop_grp[DATASET_timestamps]
+                    #print('HERE')
                     ts_indices = np.argsort(ts_ds[()])
+                    # print(ts_indices[0:10])
+                    #print ts_indices
+                    #for i in range(0, 15):
+                    #    print(ts_indices[i], ts_ds[ts_indices[i]])
+                    # exit()
                     index_ranges.append([pop_name, 0, len(ts_ds), ts_indices, pop_grp, ts_ds[ts_indices[0]]])
 
-
+            #print(index_ranges)
+            #exit()
             while index_ranges:
                 selected_r = index_ranges[0]
                 for i, r in enumerate(index_ranges[1:]):
-                    if selected_r[4] < r[4]:
+                    #print(r[5])
+                    if r[5] < selected_r[5]:
                         selected_r = r
 
+                #print(selected_r[0])
+                #exit()
+
                 ds_index = selected_r[1]
-                timestamp = pop_grp[DATASET_timestamps][ds_index]
+                timestamp = selected_r[5] # pop_grp[DATASET_timestamps][ds_index]
                 node_id = pop_grp[self._DATASET_node_ids][ds_index]
                 pop_name = selected_r[0]
                 ds_index += 1
                 if ds_index >= selected_r[2]:
+                    # print("GETS HERE")
                     index_ranges.remove(selected_r)
                 else:
                     selected_r[1] = ds_index
-                    selected_r[5] = pop_grp[DATASET_timestamps][ds_index]
+                    ts_index = selected_r[3][ds_index]
+                    #print '>>', ts_index, self._population_map[pop_name][DATASET_timestamps][selected_r[3][ds_index]]
+                    next_ts = self._population_map[pop_name][DATASET_timestamps][selected_r[3][ds_index]]
+                    # print '>>', ts_index, pop_grp[DATASET_timestamps][selected_r[3][ds_index]]
+                    selected_r[5] = next_ts #  pop_grp[DATASET_timestamps][selected_r[3][ds_index]]
 
                 yield timestamp, pop_name, node_id
 
@@ -419,7 +516,7 @@ class CSVSTReader(STReader):
 
         return self._populations
 
-    def to_dataframe(self, node_ids=None, populations=None, time_window=None, sort_by=SortOrder.none, **kwargs):
+    def to_dataframe(self, node_ids=None, populations=None, time_window=None, sort_order=SortOrder.none, **kwargs):
         selected = self._spikes_df.copy()
 
         mask = True
@@ -439,9 +536,9 @@ class CSVSTReader(STReader):
         if isinstance(mask, pd.Series):
             selected = selected[mask]
 
-        if sort_by == SortOrder.by_time:
+        if sort_order == SortOrder.by_time:
             selected.sort_values(by=col_timestamps, inplace=True)
-        elif sort_by == SortOrder.by_id:
+        elif sort_order == SortOrder.by_id:
             selected.sort_values(by=col_node_ids, inplace=True)
 
         selected.index = pd.RangeIndex(len(selected.index))
