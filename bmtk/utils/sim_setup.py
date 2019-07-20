8683aa175e5a6a9d83d06c03a1ea2a845ae06018
kaeldai <kael.dai@gmail.com>
2019-07-20 14:42:39 -0700

    updating sim_setup so it can be better used in tutorials

--- bmtk/utils/sim_setup.py
+++ bmtk/utils/sim_setup.py
@@ -28,6 +28,12 @@ import re
 from subprocess import call
 from optparse import OptionParser
 from collections import OrderedDict
+import datetime
+import logging
+from distutils.dir_util import copy_tree
+import glob
+
+logger = logging.getLogger(__name__)
 
 # Order of the different sections of the config.json. Any non-listed items will be placed at the end of the config
 config_order = [
@@ -191,11 +197,184 @@ def build_env_pointnet(base_dir='.', network_dir=None, reports=None, with_exampl
     copy_config(base_dir, simulation_config, 'simulation_config.json')
 
 
-def build_env_bionet(base_dir='.', network_dir=None, reports=None, with_examples=True, tstop=1000.0, dt=0.001,
-                     compile_mechanisms=True, **args):
+network_dir_synonyms = ['network', 'networks', 'circuit', 'circuits', 'network_dir',  'circuit_dir']
+
+
+def __get_base_dir(base_dir):
+    # Create base-dir, or if it exists make sure
+    if not os.path.exists(base_dir):
+        logger.info('Creating directory "{}"'.format(os.path.abspath(base_dir)))
+        os.mkdir(base_dir)
+    elif os.path.isfile(base_dir):
+        logging.error('simulation directory path {} points to an existing file, cannot create.'.format(base_dir))
+        exit(1)
+    else:
+        logging.info('Using existing directory "{}"'.format(os.path.abspath(base_dir)))
+
+    return os.path.abspath(base_dir)
+
+
+def __get_network_dir(base_dir, network_dir=None):
+    """Attempts to get the appropiate path the the directory containing the sonata network files, either as specified
+    by the user (-n <network_dir> argument) or else attempt to find the location of the network files.
+
+    :param base_dir:
+    :param network_dir:
+    :return: The absolute path of the directory that does/should contain the network files
+    """
+    network_dir_abs = None
+    print(base_dir)
+
+    if network_dir is None:
+        # Check to see if there are any folders in base_dir that might contain SONATA network files
+        sub_dirs = [os.path.join(base_dir, dn) for dn in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, dn))
+                    and dn.lower() in network_dir_synonyms]
+        if sub_dirs:
+            # See if there are any subfolders that might contain network files
+            network_dir_abs = os.path.abspath(sub_dirs[0])
+            logging.info('No network folder specified, attempting to use existing folder: {}'.format(network_dir_abs))
+        else:
+            network_dir_abs = os.path.abspath(os.path.join(base_dir, 'network'))
+            logging.info('No network folder specified, creating empty folder: {}'.format(network_dir_abs))
+            os.makedirs(network_dir_abs)
+
+    elif os.path.isabs(network_dir):
+        # In the case the user specifies the network directory as an absolute path
+        network_dir_abs = network_dir
+        logging.info('Using network directory: {}'.format(network_dir_abs))
+
+    elif os.path.isdir(network_dir):
+        # In case during calling the script the user was pointing to an already existing directory
+        network_dir_abs = os.path.abspath(network_dir)
+        logging.info('Using network directory: {}'.format(network_dir_abs))
+
+    else:
+        # Default case is the to use/add the <network_dir> found under base_dir
+        network_dir_abs = os.path.join(base_dir, network_dir)
+        if not os.path.exists(network_dir_abs):
+            logging.info('Creating new network directory: {}'.format(network_dir_abs))
+            os.makedirs(network_dir_abs)
+        else:
+            logging.info('Using network directory: {}'.format(network_dir_abs))
+
+    return network_dir_abs
+
+
+def __parse_network_dir(network_dir):
+    logger.info('Parsing {} for SONATA network files'.format(network_dir))
+    net_nodes = {}
+    net_edges = {}
+    for root, dirs, files in os.walk(network_dir):
+        for f in files:
+            if not os.path.isfile(os.path.join(network_dir, f)) or f.startswith('.'):
+                continue
+
+            if '_nodes' in f:
+                net_name = f[:f.find('_nodes')]
+                nodes_dict = net_nodes.get(net_name, {})
+                nodes_dict['nodes_file'] = os.path.abspath(os.path.join(root, f))
+                net_nodes[net_name] = nodes_dict
+                logger.info('  Adding nodes file: {}'.format(nodes_dict['nodes_file']))
+
+            elif '_node_types' in f:
+                net_name = f[:f.find('_node_types')]
+                nodes_dict = net_nodes.get(net_name, {})
+                nodes_dict['node_types_file'] = os.path.abspath(os.path.join(root, f))
+                net_nodes[net_name] = nodes_dict
+                logger.info('  Adding node types file: {}'.format(nodes_dict['node_types_file']))
+
+            elif '_edges' in f:
+                net_name = f[:f.find('_edges')]
+                edges_dict = net_edges.get(net_name, {})
+                edges_dict['edges_file'] = os.path.abspath(os.path.join(root, f))
+                net_edges[net_name] = edges_dict
+                logger.info('  Adding edges file: {}'.format(edges_dict['edges_file']))
+
+            elif '_edge_types' in f:
+                net_name = f[:f.find('_edge_types')]
+                edges_dict = net_edges.get(net_name, {})
+                edges_dict['edge_types_file'] = os.path.abspath(os.path.join(root, f))
+                net_edges[net_name] = edges_dict
+                logger.info('  Adding edge types file: {}'.format(edges_dict['edges_file']))
+
+            else:
+                logger.info('  Skipping file (could not categorize): {}'.format(os.path.abspath(os.path.join(root, f))))
+
+    if not (net_nodes or net_edges):
+        logger.info('  Could not find any sonata nodes or edges file(s).')
+
+    network_config = {'nodes': [], 'edges': []}
+    for _, sect in net_nodes.items():
+        network_config['nodes'].append(sect)
+
+    for _, sect in net_edges.items():
+        network_config['edges'].append(sect)
+
+    return network_config
+
+
+def __get_components_dir(base_dir, components_dir=None):
+    if components_dir is None:
+        return os.path.abspath(os.path.join(base_dir, 'components'))
+
+    elif os.path.isabs(components_dir):
+        return components_dir
+
+    elif os.path.exists(components_dir):
+        return os.path.abspath(components_dir)
+
+    else:
+        return os.path.abspath(os.path.join(base_dir, components_dir))
+
+
+def __create_components_dir(components_dir, comp_root, copy_files=True):
+    if not os.path.exists(components_dir):
+        logger.info('Creating components directory: {}'.format(components_dir))
+        os.makedirs(components_dir)
+
+    components_config = {}
+
+    comps_dirs = [sd for sd in os.listdir(comp_root) if os.path.isdir(os.path.join(comp_root, sd))]
+    print(components_dir)
+    for sub_dir in comps_dirs:
+        comp_name = sub_dir + '_dir'
+        src_dir = os.path.join(comp_root, sub_dir)
+        trg_dir = os.path.join(components_dir, sub_dir)
+        if not os.path.exists(trg_dir):
+            logger.info('Creating new components directory: {}'.format(trg_dir))
+            os.makedirs(trg_dir)
+        else:
+            logger.info('Using components directory: {}'.format(trg_dir))
+
+        components_config[comp_name] = trg_dir
+
+        if copy_files:
+            logger.info('  Copying files from {}.'.format(src_dir))
+            copy_tree(src_dir, trg_dir)
+
+    return components_config
+
+def build_env_bionet(base_dir='.', network_dir=None, components_dir=None,reports=None, with_examples=True, tstop=1000.0,
+                     dt=0.001, compile_mechanisms=True, **args):
+    logger.info('Creating BioNet simulation environment ({})'.format(datetime.datetime.now()))
     simulator='bionet'
     target_simulator='NEURON'
-    components_dir='biophys_components'
+    # components_dir='biophys_components'
+
+    circuit_config = {}
+
+    parsed_base_dir = __get_base_dir(base_dir)
+
+    parsed_network_dir = __get_network_dir(parsed_base_dir, network_dir)
+    circuit_config['networks'] = __parse_network_dir(parsed_network_dir)
+
+    parsed_components_dir = __get_components_dir(base_dir, components_dir)
+    circuit_config['components'] = __create_components_dir(parsed_components_dir,
+                                                           comp_root=os.path.join(scripts_path, 'bionet'),
+                                                           copy_files=with_examples)
+
+    exit()
+
 
     # Copy run script
     copy_run_script(base_dir=base_dir, simulator=simulator, run_script='run_{}.py'.format(simulator))
@@ -259,119 +438,11 @@ def build_env_popnet(base_dir='.', network_dir=None, reports=None, with_examples
     copy_config(base_dir, simulation_config, 'simulation_config.json')
 
 
-"""
-def build_env_bionet(base_dir='.', run_time=0.0, with_config=True, network_dir=None, with_cell_types=True,
-                     compile_mechanisms=True, reports=None):
-    local_path = os.path.dirname(os.path.realpath(__file__))
-    scripts_path = os.path.join(local_path, 'scripts', 'bionet')
-
-    components_dir = os.path.join(base_dir, 'components')
-    component_paths = {
-        'morphologies_dir': os.path.join(components_dir, 'biophysical', 'morphology'),
-        'biophysical_models_dir': os.path.join(components_dir, 'biophysical', 'electrophysiology'),
-        'mechanisms_dir': os.path.join(components_dir, 'mechanisms'),
-        'point_models_dir': os.path.join(components_dir, 'intfire'),
-        'synaptic_models_dir': os.path.join(components_dir, 'synaptic_models'),
-        'templates_dir': os.path.join(components_dir, 'hoc_templates')
-    }
-    for path in component_paths.values():
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-    if with_cell_types:
-        shutil.rmtree(component_paths['templates_dir'])
-        shutil.copytree(os.path.join(scripts_path, 'hoc_templates'), component_paths['templates_dir'])
-
-        shutil.rmtree(component_paths['mechanisms_dir'])
-        shutil.copytree(os.path.join(scripts_path, 'mechanisms'), component_paths['mechanisms_dir'])
-
-        shutil.rmtree(component_paths['synaptic_models_dir'])
-        shutil.copytree(os.path.join(scripts_path, 'synaptic_models'), component_paths['synaptic_models_dir'])
-
-        shutil.rmtree(component_paths['point_models_dir'])
-        shutil.copytree(os.path.join(scripts_path, 'intfire'), component_paths['point_models_dir'])
-
-        if compile_mechanisms:
-            cwd = os.getcwd()
-            os.chdir(component_paths['mechanisms_dir'])
-            try:
-                print(os.getcwd())
-                call(['nrnivmodl', 'modfiles'])
-            except Exception as e:
-                print('Was unable to compile mechanism in {}'.format(component_paths['mechanisms_dir']))
-                # print e.message
-            os.chdir(cwd)
-
-        shutil.copy(os.path.join(scripts_path, 'run_bionet.py'), os.path.join(base_dir, 'run_bionet.py'))
-
-    if with_config:
-        config_json = json.load(open(os.path.join(scripts_path, 'default_config.json')))
-        config_json['manifest']['$BASE_DIR'] = os.path.abspath(base_dir)
-        config_json['manifest']['$COMPONENTS_DIR'] = os.path.join('${BASE_DIR}', 'components')
-        config_json['run']['tstop'] = run_time
-
-        if network_dir is not None:
-            config_json['manifest']['$NETWORK_DIR'] = os.path.abspath(network_dir)
-
-            net_nodes = {}
-            net_edges = {}
-            for f in os.listdir(network_dir):
-                if not os.path.isfile(os.path.join(network_dir, f)) or f.startswith('.'):
-                    continue
-
-                if '_nodes' in f:
-                    net_name = f[:f.find('_nodes')]
-                    nodes_dict = net_nodes.get(net_name, {'name': net_name})
-                    nodes_dict['nodes_file'] = os.path.join('${NETWORK_DIR}', f)
-                    net_nodes[net_name] = nodes_dict
-
-                elif '_node_types' in f:
-                    net_name = f[:f.find('_node_types')]
-                    nodes_dict = net_nodes.get(net_name, {'name': net_name})
-                    nodes_dict['node_types_file'] = os.path.join('${NETWORK_DIR}', f)
-                    net_nodes[net_name] = nodes_dict
-
-                elif '_edges' in f:
-                    net_name = f[:f.find('_edges')]
-                    edges_dict = net_edges.get(net_name, {'name': net_name})
-                    edges_dict['edges_file'] = os.path.join('${NETWORK_DIR}', f)
-                    try:
-                        edges_h5 = h5py.File(os.path.join(network_dir, f), 'r')
-                        edges_dict['target'] = edges_h5['edges']['target_gid'].attrs['network']
-                        edges_dict['source'] = edges_h5['edges']['source_gid'].attrs['network']
-                    except Exception as e:
-                        pass
-
-                    net_edges[net_name] = edges_dict
-
-                elif '_edge_types' in f:
-                    net_name = f[:f.find('_edge_types')]
-                    edges_dict = net_edges.get(net_name, {'name': net_name})
-                    edges_dict['edge_types_file'] = os.path.join('${NETWORK_DIR}', f)
-                    net_edges[net_name] = edges_dict
-
-                else:
-                    print('Unknown file {}. Will have to enter by hand'.format(f))
-
-            for _, sect in net_nodes.items():
-                config_json['networks']['nodes'].append(sect)
-
-            for _, sect in net_edges.items():
-                config_json['networks']['edges'].append(sect)
-
-        if reports is not None:
-            for report_name, report_params in reports.items():
-                config_json['reports'][report_name] = report_params
-
-        ordered_dict = OrderedDict(sorted(config_json.items(),
-                                          key=lambda s: config_order.index(s[0]) if s[0] in config_order else 100))
-        with open(os.path.join(base_dir, 'config.json'), 'w') as outfile:
-            json.dump(ordered_dict, outfile, indent=2)
-            #json.dump(config_json, outfile, indent=2)
-"""
-
-
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(module)s [%(levelname)s] %(message)s')
+    #logger.setLevel(level=logging.INFO)
+    # logger.setFormatter('%(asctime)s [%(levelname)s] %(message)s')
+
     def str_list(option, opt, value, parser):
         setattr(parser.values, option.dest, value.split(','))
 
@@ -385,8 +456,8 @@ if __name__ == '__main__':
             setattr(parser.values, option.dest, value)
 
 
-    parser = OptionParser(usage="Usage: python -m bmtk.utils.sim_setup [options] bionet|pointnet|popnet|mintnet")
-    parser.add_option('-b', '--base_dir', dest='base_dir', default='.', help='path of environment')
+    parser = OptionParser(usage="Usage: python %prog [options] [bionet|pointnet|popnet|filternet] sim_dir")
+    # parser.add_option('-b', '--base_dir', dest='base_dir', default='.', help='path of environment')
     parser.add_option('-n', '--network_dir', dest='network_dir', default=None,
                       help="Use an exsting directory with network files.")
     parser.add_option('-r', '--tstop', type='float', dest='tstop', default=1000.0)
@@ -415,6 +486,8 @@ if __name__ == '__main__':
                       callback=membrane_report_parser, default='all')
 
     options, args = parser.parse_args()
+
+
     reports = {}
 
     if options.has_membrane_report:
@@ -426,18 +499,26 @@ if __name__ == '__main__':
             'sections': options.mem_rep_secs,
         }
 
-    target_sim = args[0].lower() if len(args) == 1 else None
-    if target_sim not in ['bionet', 'popnet', 'pointnet', 'mintnet']:
-        raise Exception('Must specify one target simulator. options: "bionet", pointnet", "popnet" or "mintnet"')
+    # Check the passed in argments are correct. [sim] </path/to/dir/>
+    if len(args) < 2:
+        parser.error('Invalid number of arguments, Please specify a target simulation (bionet, pointnet, filternet,'
+                     'popnet) and the path to save the simulation environment.')
+    elif len(args) > 2:
+        parser.error('Unrecognized arguments {}'.format(args[2:]))
+    else:
+        target_sim = args[0].lower()
+        if target_sim not in ['bionet', 'popnet', 'pointnet', 'mintnet']:
+            parser.error('Must specify one target simulator. options: "bionet", pointnet", "popnet", "filternet"')
+        base_dir = args[1]
 
     if target_sim == 'bionet':
-        build_env_bionet(base_dir=options.base_dir, network_dir=options.network_dir, tstop=options.tstop,
+        build_env_bionet(base_dir=base_dir, network_dir=options.network_dir, tstop=options.tstop,
                          dt=options.dt, reports=reports)
 
     elif target_sim == 'pointnet':
-        build_env_pointnet(base_dir=options.base_dir, network_dir=options.network_dir, tstop=options.tstop,
+        build_env_pointnet(base_dir=base_dir, network_dir=options.network_dir, tstop=options.tstop,
                            dt=options.dt, reports=reports)
 
     elif target_sim == 'popnet':
-        build_env_popnet(base_dir=options.base_dir, network_dir=options.network_dir, tstop=options.tstop,
+        build_env_popnet(base_dir=base_dir, network_dir=options.network_dir, tstop=options.tstop,
                            dt=options.dt, reports=reports)
