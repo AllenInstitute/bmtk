72a07f0c067caab845680c392a0b6d204ac0aa1f
kaeldai <kael.dai@gmail.com>
2019-10-26 12:39:45 -0700

    adding ability to save resulting weights when running on mulitple nodes

--- bmtk/simulator/core/simulator_network.py
+++ bmtk/simulator/core/simulator_network.py
@@ -1,4 +1,5 @@
 from six import string_types
+import numpy as np
 
 from bmtk.simulator.core.io_tools import io
 #from bmtk.simulator.core.config import ConfigDict
@@ -95,13 +96,29 @@ class SimNetwork(object):
         # Used in inputs/reports when needed to get all gids belonging to a node population
         self._node_sets[pop_name] = NodeSet({'population': pop_name}, self)
 
-    def node_properties(self):
-        node_props = {}
-        for node_pop in self.node_populations:
-            if node_pop.internal_nodes_only:
-                node_props[node_pop.name] = node_pop.nodes_df()
+    def node_properties(self, populations=None):
+        if populations is None:
+            selected_pops = self.node_populations
 
-        return node_props
+        elif isinstance(populations, string_types):
+            selected_pops = [pop for pop in self.node_populations if pop.name == populations]
+
+        else:
+            selected_pops = [pop for pop in self.node_populations if pop.name in populations]
+
+        all_nodes_df = None
+        for node_pop in selected_pops:
+            node_pop_df = node_pop.nodes_df()
+            if 'population' not in node_pop_df:
+                node_pop_df['population'] = node_pop.name
+
+            node_pop_df = node_pop_df.set_index(['population', node_pop_df.index.astype(dtype=np.uint64)])
+            if all_nodes_df is None:
+                all_nodes_df = node_pop_df
+            else:
+                all_nodes_df = all_nodes_df.append(node_pop_df)
+
+        return all_nodes_df
 
     def add_edges(self, edge_population):
         edge_population.initialize(self)
