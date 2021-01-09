da2a6fde96557869b5b8214b9ac1eaf2ffba11f9
kaeldai <kael.dai@gmail.com>
2021-01-08 16:33:59 -0800

    updating docker image for latest version of bmtk

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -3,13 +3,16 @@ set -e
 
 if [[ $1 = "nrnivmodl" ]]; then
     shift
+    cd /home/shared/workspace/mechanisms
     nrnivmodl $@
 elif [[ $1 = "python" ]]; then
     shift
+    cd /home/shared/workspace
     python $@
 elif [[ $1 = "jupyter" ]]; then
     shift
     jupyter notebook --allow-root --ip=* --port 8888 --no-browser --notebook-dir /home/shared --NotebookApp.token=""
 else
+    cd /home/shared/workspace
     python $@
 fi
