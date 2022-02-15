c1f285cbfe6909dae21102d22707bce167ff6808
kaeldai <kael.dai@gmail.com>
2022-02-15 10:33:10 -0800

    updating popnet examples

--- docs/examples/pop_2pops/build_network.py
+++ docs/examples/pop_2pops/build_network.py
@@ -1,48 +1,60 @@
 from bmtk.builder import NetworkBuilder
 
-net = NetworkBuilder('brunel')
-net.add_nodes(pop_name='excitatory',
-              ei='e',
-              model_type='population',
-              model_template='dipde:Internal',
-              dynamics_params='exc_model.json')
+# Create internal/recurrent populations of nodes
+internal_net = NetworkBuilder('internal')
+internal_net.add_nodes(
+    pop_name='excitatory',
+    ei='e',
+    model_type='population',
+    model_template='dipde:Internal',
+    dynamics_params='exc_model.json'
+)
 
-net.add_nodes(pop_name='inhibitory',
-              ei='i',
-              model_type='population',
-              model_template='dipde:Internal',
-              dynamics_params='inh_model.json')
+internal_net.add_nodes(
+    pop_name='inhibitory',
+    ei='i',
+    model_type='population',
+    model_template='dipde:Internal',
+    dynamics_params='inh_model.json'
+)
 
-net.add_edges(source={'ei': 'e'}, target={'ei': 'i'},
-              syn_weight=0.005,
-              nsyns=20,
-              delay=0.002,
-              dynamics_params='ExcToInh.json')
+# Recurrently connect internal populations
+internal_net.add_edges(
+    source={'ei': 'e'}, target={'ei': 'i'},
+    syn_weight=0.005,
+    nsyns=20,
+    delay=0.002,
+    dynamics_params='ExcToInh.json'
+)
 
-net.add_edges(source={'ei': 'i'}, target={'ei': 'e'},
-              syn_weight=-0.002,
-              nsyns=10,
-              delay=0.002,
-              dynamics_params='InhToExc.json')
+internal_net.add_edges(
+    source={'ei': 'i'}, target={'ei': 'e'},
+    syn_weight=-0.002,
+    nsyns=10,
+    delay=0.002,
+    dynamics_params='InhToExc.json'
+)
 
-net.build()
-net.save_nodes(nodes_file_name='brunel_nodes.h5', node_types_file_name='brunel_node_types.csv', output_dir='network')
-net.save_edges(edges_file_name='brunel_edges.h5', edge_types_file_name='brunel_edge_types.csv', output_dir='network')
+internal_net.build()
+internal_net.save(output_dir='network')
 
 
-input_net = NetworkBuilder('inputs')
-input_net.add_nodes(pop_name='tON',
-                    ei='e',
-                    model_type='virtual')
+# Create external pops
+external_net = NetworkBuilder('external')
+external_net.add_nodes(
+    pop_name='tON',
+    ei='e',
+    model_type='virtual'
+)
 
-input_net.add_edges(target=net.nodes(ei='e'),
-                    syn_weight=0.0025,
-                    nsyns=10,
-                    delay=0.002,
-                    dynamics_params='input_ExcToExc.json')
+# Connect external pop to internal pops
+external_net.add_edges(
+    target=internal_net.nodes(ei='e'),
+    syn_weight=0.0025,
+    nsyns=10,
+    delay=0.002,
+    dynamics_params='input_ExcToExc.json'
+)
 
-input_net.build()
-input_net.save_nodes(nodes_file_name='input_nodes.h5', node_types_file_name='input_node_types.csv',
-                     output_dir='network')
-input_net.save_edges(edges_file_name='input_edges.h5', edge_types_file_name='input_edge_types.csv',
-                     output_dir='network')
+external_net.build()
+external_net.save(output_dir='network')
--- docs/examples/pop_7pops_converted/run_popnet.py
+++ docs/examples/pop_7pops_converted/run_popnet.py
@@ -5,6 +5,7 @@ from bmtk.simulator import popnet
 def main(config_file):
     configure = popnet.config.from_json(config_file)
     configure.build_env()
+
     network = popnet.PopNetwork.from_config(configure, group_by='node_type_id')
     sim = popnet.PopSimulator.from_config(configure, network)
     sim.run()
@@ -14,4 +15,4 @@ if __name__ == '__main__':
     if __file__ != sys.argv[-1]:
         main(sys.argv[-1])
     else:
-        main('config.json')
+        main('config.simulation.json')
