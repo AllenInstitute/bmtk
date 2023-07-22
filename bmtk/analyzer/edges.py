3a1b74ca2a36af3b53004d4f56a521f30efda105
kaeldai <kael.dai@gmail.com>
2023-07-21 22:22:43 -0700

    adding edge statistic functionality

---
+++ bmtk/analyzer/edges.py
@@ -0,0 +1,12 @@
+from bmtk.utils.sonata.edge_stats import (
+    to_edges_dataframe, 
+    edge_props_distribution,
+    nsyns_distribution,
+    nconnections_distributions,
+    plot_distribution,
+    plot_correlation,
+    edge_stats_table,
+    pearson_r,
+    chisquare,
+    kolmogorov_smirnov
+)
