60b6af6fc6c54fac34c480d0b76ca151e99d3e3f
Tyler Banks <tbanks@mail.missouri.edu>
2023-10-04 14:54:06 -0500

    mpi iclamp fix

--- bmtk/simulator/bionet/modules/iclamp.py
+++ bmtk/simulator/bionet/modules/iclamp.py
@@ -133,7 +133,12 @@ class IClampMod(iclamp.IClampMod):
     def initialize(self, sim):
         # Get select node gids, but only for those nodes that are on the current rank (if running on multiple cores)
         select_gids = list(sim.net.get_node_set(self._node_set).gids())
+<<<<<<< HEAD
         gids_on_rank = list(set(select_gids) & set(sim.local_gids))
+=======
+        local_gids = sim.net.get_local_cells()
+        gids_on_rank = list(set(select_gids) & set(local_gids))
+>>>>>>> 0e3fef4e (mpi iclamp fix)
 
         for gid in gids_on_rank:           
             cell = sim.net.get_cell_gid(gid)
