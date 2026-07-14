3d4acd2b66d2662a6f1af1c8c8c9b4218dfb880a
Kael Dai <kael.dai@gmail.com>
2026-07-13 18:57:25 -0700

    adding reset option for workshop

--- bmtk/simulator/dpointnet/__init__.py
+++ bmtk/simulator/dpointnet/__init__.py
@@ -8,3 +8,8 @@ from .config import Config
 from .input_modules import InputModules
 from .loss_functions import register_loss_module, add_loss_module
 from .tf_utils import cleanup_tensorflow, enable_gpu_memory_growth
+from .id_maps import TFIDMap
+
+
+def reset():
+    TFIDMap().reset()
